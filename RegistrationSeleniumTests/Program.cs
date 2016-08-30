0be7a513aa04cd90b4b604e09fc3557c5196a7e4
Paul <gh@wojcik.com.au>
2016-08-30 19:07:06 +1000

    Initial boiler plate code

--- RegistrationSeleniumTests/Program.cs
+++ RegistrationSeleniumTests/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace RegistrationSeleniumTests
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://register.training.business.gov.au");
+
+                var dismiss = driver.FindElement(By.XPath("/html/body/div[2]/button"));
+                dismiss.Click();
+
+                Console.WriteLine("Press enter to continue...");
+                Console.ReadLine();
+            }
         }
     }
 }
